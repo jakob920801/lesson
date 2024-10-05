9cf989588cc8a9acb4ce4bfcd9c1b2fe38bc71d8
jakob920801 <rnjsehdbs92@gmail.com>
2024-10-05 12:22:02 +0900

    featA_num

--- testA.js
+++ testA.js
@@ -4,7 +4,17 @@ function test(text) {
   console.log(text);
 }
 
-test(alpha);
-test("hi");
+// test(alpha);
+// test("hi");
 
 // const -> let
+
+function num() {
+  if (Math.random() < 0.5) {
+    return "hello";
+  } else {
+    return "hi";
+  }
+}
+
+console.log(num());
